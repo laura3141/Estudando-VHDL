bf4dec072990396ece74ea118950ad5e6b129388
Laura <laura.costa3141@gmail.com>
2024-05-07 20:58:29 -0300

    m

--- somador-completo/design2.vhd
+++ somador-completo/design2.vhd
@@ -1,90 +1,52 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity somador_completo_tb is
-end somador_completo_tb;
+--outra entidade distinta da anterior
+entity somador_completo is
+    port(
+        a: in std_logic;
+        b: in std_logic;
+        carry_in: in std_logic;
+        soma: out std_logic;
+        carry_out: out std_logic
+    );
+end somador_completo;
 
-architecture testbench of somador_completo_tb is
-    -- componente interno da arquitetura de teste
-    component somador_completo
+architecture somador_completo_arch of somador_completo is
+	--componente interno da arquitetura do somador completo
+    component meio_somador is
         port(
             a: in std_logic;
             b: in std_logic;
-            carry_in: in std_logic;
             soma: out std_logic;
-            carry_out: out std_logic
+            carry: out std_logic
         );
     end component;
 
-    -- entradas como sinais
-    signal a_tb, b_tb, carry_in_tb: std_logic;
-    signal soma_tb, carry_out_tb: std_logic;
-    
+    -- sinais internos
+    signal S_primeira_soma: std_logic;
+    signal S_primeiro_carry: std_logic;
+    signal S_segunda_soma: std_logic;
+    signal S_segundo_carry: std_logic;
 begin
-
-   
-    uut: somador_completo 
-        port map (
-            a => a_tb,
-            b => b_tb,
-            carry_in => carry_in_tb,
-            soma => soma_tb,
-            carry_out => carry_out_tb
+    --somador 1 é uma instancia de meio somador
+    somador1: meio_somador
+        port map(
+            a => a, --os termos a esquerda sao a mascara, os da direita a instancia
+            b => b,
+            soma => S_primeira_soma,
+            carry => S_primeiro_carry
         );
 
-    -- inicio do processo de estimulo
-    stimulus: process
-    begin
-        -- Test Case 1
-        a_tb <= '0';
-        b_tb <= '0';
-        carry_in_tb <= '0';
-        wait for 10 ns;
-        
-        -- Test Case 2
-        a_tb <= '1';
-        b_tb <= '0';
-        carry_in_tb <= '0';
-        wait for 10 ns;
-        
-        -- Test Case 3
-        a_tb <= '0';
-        b_tb <= '1';
-        carry_in_tb <= '0';
-        wait for 10 ns;
-        
-        -- Test Case 4
-        a_tb <= '1';
-        b_tb <= '1';
-        carry_in_tb <= '0';
-        wait for 10 ns;
-        
-        -- Test Case 5
-        a_tb <= '0';
-        b_tb <= '0';
-        carry_in_tb <= '1';
-        wait for 10 ns;
-        
-        -- Test Case 6
-        a_tb <= '1';
-        b_tb <= '0';
-        carry_in_tb <= '1';
-        wait for 10 ns;
-        
-        -- Test Case 7
-        a_tb <= '0';
-        b_tb <= '1';
-        carry_in_tb <= '1';
-        wait for 10 ns;
-        
-        -- Test Case 8
-        a_tb <= '1';
-        b_tb <= '1';
-        carry_in_tb <= '1';
-        wait for 10 ns;
-        
-        -- End of testbench
-        wait;
-    end process stimulus;
+    
+    somador2: meio_somador
+        port map(
+            a => S_primeira_soma,
+            b => carry_in,
+            soma => soma,
+            carry => S_segundo_carry
+        );
 
-end testbench;
+    
+    carry_out <= S_primeiro_carry or S_segundo_carry;
+end somador_completo_arch;
